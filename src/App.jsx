82c8e3aa344e0397f6fbb6609134480deee5da96
munibofficial <114603371+munibofficial@users.noreply.github.com>
2023-07-06 12:49:25 +0500

    1 file modified, 2 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-const people = [
-  {}
+// // export default function button() {
+// //   return (
+// //     <>
+// //       <button onClick={Alert}>Nothing</button>
+// //       <button onDoubleClick ={Alert1}>Hover</button>
+// //     </>
+// //   );
+// // }
+// // function Alert() {
+// //   return alert('I am clicked');
+// // }
+// // function Alert1() {
+// //   return alert('I am hover');
+// // }
+// // function AlertButton({ message, children }) {
+// //   return <button onclick={() => alert(message)}></button>;
+// //   return <button onClick={() => alert(message)}>{children}</button>;
+// // }
 
-];
+// export default function Toolbar() {
+//   return (
+//     <div>
+//       <PlayButton moviename="Carry On Jutt">Play movie</PlayButton>
+//     </div>
+//   );
+// }
 
-export default function List() {
-  const listItems = people.map(person =>
-    <li>{person}</li>
-  );
-  return <ul>{listItems}</ul>;
-}
+// function Button({ onClick, children }) {
+//   return <button onClick={onClick}>{children}</button>;
+// }
+
+// function PlayButton({ moviename, children }) {
+//   function handlePlayButton() {
+//     alert(`Playing ${moviename}`);
+//   }
+//   return <Button onClick={handlePlayButton}>Play {moviename}</Button>;
+// }
