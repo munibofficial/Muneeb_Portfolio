8286a434b0d5ae3ab57272bd59fe9da4871f49cd
munibofficial <114603371+munibofficial@users.noreply.github.com>
2023-07-08 01:24:06 +0500

    1 binaries uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -38,12 +38,13 @@
 import Header from './Header.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AvatarComponent from './Avatar.jsx';
+import Image from "./images/waleed.jpeg"
 
 export default function App() {
   return (
     <div className="header">
       <Header />
-      <AvatarComponent name="Muneeb Amjad"  />
+      <AvatarComponent name="Muneeb Amjad" avatarUrl={Image} />
     </div>
   );
 }
--- src/Avatar.jsx
+++ src/Avatar.jsx
@@ -3,7 +3,21 @@ import React from 'react';
 function AvatarComponent({ name, avatarUrl }) {
   return (
     <div style={{ textAlign: 'center' }}>
-      <img src={avatarUrl} alt={name} style={{ width: '150px', borderRadius: '50%' }} />
+      <div
+        style={{
+          width: '150px',
+          height: '150px',
+          overflow: 'hidden',
+          borderRadius: '50%',
+          margin: '0 auto',
+        }}
+      >
+        <img
+          src={avatarUrl}
+          alt={name}
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        />
+      </div>
       <h2>{name}</h2>
     </div>
   );
